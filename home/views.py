884f84c971584e95149232f54f1e7fb0db7113a6
Crowdbotics <team@crowdbotics.com>
2019-11-21 16:59:31 +0000

    add package stripe

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'stripe', 'url': 'http://pypi.python.org/pypi/stripe/1.77.1'},
 	{'name':'boto3', 'url': 'http://pypi.python.org/pypi/boto3/1.5.15'},
 	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
 	{'name':'django-bootstrap4', 'url': 'https://pypi.org/project/django-bootstrap4/0.0.7/'},
