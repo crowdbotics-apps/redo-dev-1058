c82027109fd58b560dd52fae4b56b4c7d0962448
Crowdbotics <team@crowdbotics.com>
2020-03-26 16:04:22 +0000

    Update models

--- home/views.py
+++ home/views.py
@@ -1,21 +1,28 @@
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
-
-# Create your views here.
-
-from home.models import CustomText, HomePage
+from .models import HomePage, Testtt, Test, CustomText, Testing
 
 
 def home(request):
     packages = [
-	{'name':'stripe', 'url': 'http://pypi.python.org/pypi/stripe/1.77.1'},
-	{'name':'boto3', 'url': 'http://pypi.python.org/pypi/boto3/1.5.15'},
-	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
-	{'name':'django-bootstrap4', 'url': 'https://pypi.org/project/django-bootstrap4/0.0.7/'},
-	{'name':'djangorestframework', 'url': 'https://pypi.org/project/djangorestframework/3.9.0/'},
+        {"name": "stripe", "url": "http://pypi.python.org/pypi/stripe/1.77.1"},
+        {"name": "boto3", "url": "http://pypi.python.org/pypi/boto3/1.5.15"},
+        {
+            "name": "django-allauth",
+            "url": "https://pypi.org/project/django-allauth/0.38.0/",
+        },
+        {
+            "name": "django-bootstrap4",
+            "url": "https://pypi.org/project/django-bootstrap4/0.0.7/",
+        },
+        {
+            "name": "djangorestframework",
+            "url": "https://pypi.org/project/djangorestframework/3.9.0/",
+        },
     ]
     context = {
-        'customtext': CustomText.objects.first(),
-        'homepage': HomePage.objects.first(),
-        'packages': packages
+        "customtext": CustomText.objects.first(),
+        "homepage": HomePage.objects.first(),
+        "packages": packages,
     }
-    return render(request, 'home/index.html', context)
+    return render(request, "home/index.html", context)
